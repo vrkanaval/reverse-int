f291281e7746f8a0543ccbe95649043422566ca9
vrkanaval <veronica22.199@gmail.com>
2023-10-05 12:38:34 +0400

    feat: add solution for reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n *= -1;
+      }
+
+      let reversedNum = 0;
+
+      while (n > 0) {
+        reversedNum = reversedNum * 10 + n % 10;
+        n = Math.floor(n / 10);
+      }
+    
+      return reversedNum;
+
 }
